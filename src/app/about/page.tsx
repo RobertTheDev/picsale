86096d09523ea218910c84274482f791d0d8ab2c
Robert Hawker <robertthedev@gmail.com>
2024-12-18 15:15:17 +0000

    chore: set up new next js project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the aboutt page</p>
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
