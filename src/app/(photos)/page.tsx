7a77bd034f8ceeea3bae069ec02ccffb5432c260
Robert Hawker <robertthedev@gmail.com>
2024-12-19 13:07:19 +0000

    feat: added photo filter menu component

--- src/app/(photos)/page.tsx
+++ src/app/(photos)/page.tsx
@@ -1,14 +1,17 @@
-import { photos } from '../data/photos';
-import PhotoProductCard from './components/PhotoProductCard';
+import PhotoFilterMenu from './components/PhotoFilterMenu';
+import PhotoProductsList from './components/PhotoProductsList';
 
 export default function PhotosPage() {
   return (
     <main className="w-full bg-gray-50">
-      <ul className="grid w-full grid-cols-4 gap-8 p-8">
-        {photos.map((photo) => {
-          return <PhotoProductCard key={photo.id} photo={photo} />;
-        })}
-      </ul>
+      <div className="flex flex-1">
+        <div className="fixed left-0 h-full w-80 bg-white p-8">
+          <PhotoFilterMenu />
+        </div>
+        <div className="flex w-full flex-1 flex-col pl-80">
+          <PhotoProductsList />
+        </div>
+      </div>
     </main>
   );
 }
