d9568f3e89aba262b81e743fbc16dbd64eeb5f94
Robert Hawker <robertthedev@gmail.com>
2024-12-19 07:40:24 +0000

    feat: added photo product page link to card component

--- src/app/(photos)/components/PhotoProductCard/index.tsx
+++ src/app/(photos)/components/PhotoProductCard/index.tsx
@@ -1,12 +1,15 @@
 import IPhoto from '@/app/interfaces/Photo';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function PhotoProductCard({ photo }: { photo: IPhoto }) {
   return (
     <li className="w-full">
-      <div className="relative aspect-square w-full">
-        <Image fill objectFit="cover" alt="" src={`/photos/${photo.url}`} />
-      </div>
+      <Link href={`/photos/${photo.id}`}>
+        <div className="relative aspect-square w-full">
+          <Image fill objectFit="cover" alt="" src={`/photos/${photo.url}`} />
+        </div>
+      </Link>
     </li>
   );
 }
