63114246a552bd368fb26b1ecc5f03332868b148
Robert Hawker <robertthedev@gmail.com>
2024-12-19 08:14:39 +0000

    chore: added photos home redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/photos',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
