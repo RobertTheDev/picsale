ab62b25b0d8626a1332f9496c607e2d73f20aff9
Robert Hawker <robertthedev@gmail.com>
2024-12-19 10:43:28 +0000

    feat: created and added header profile menu component

---
+++ src/app/layout/Header/components/HeaderProfileMenu/index.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { useOutsideClick } from '@/app/hooks/useOutsideClick';
+import Image from 'next/image';
+import Link from 'next/link';
+import { LuCog, LuHeart, LuHistory, LuImages, LuUser } from 'react-icons/lu';
+
+export default function HeaderProfileMenu() {
+  const { targetActive, toggleTarget, toggleRef, targetRef } =
+    useOutsideClick();
+
+  return (
+    <div className="relative">
+      <button ref={toggleRef} onClick={toggleTarget}>
+        <LuUser size={20} />
+      </button>
+      {targetActive && (
+        <div
+          ref={targetRef}
+          className="absolute right-0 top-20 flex w-64 flex-col items-center rounded-lg bg-white p-8 shadow-md"
+        >
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+              <div className="relative h-16 w-16 overflow-hidden rounded-full">
+                <Image fill alt="" src="/avatar.jpg" />
+              </div>
+              <p className="mt-2">Robert</p>
+            </div>
+            <div className="mt-4">
+              <ul className="flex flex-col gap-4">
+                <li>
+                  <Link className="flex items-center" href="/orders">
+                    <div className="flex items-center gap-2">
+                      <LuHistory size={20} />
+                      <p>Orders</p>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="flex items-center" href="/edit-profile">
+                    <div className="flex items-center gap-2">
+                      <LuCog size={20} />
+                      <p>Account Settings</p>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="flex items-center" href="/wishlist">
+                    <div className="flex items-center gap-2">
+                      <LuHeart size={20} />
+                      <p>Wishlist</p>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="flex items-center" href="/my-listings">
+                    <div className="flex items-center gap-2">
+                      <LuImages size={20} />
+                      <p>My Listings</p>
+                    </div>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mt-4">
+              <p className="underline">Sign Out</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -1,5 +1,29 @@
+import Link from 'next/link';
+import { LuShoppingBasket } from 'react-icons/lu';
+import HeaderProfileMenu from './components/HeaderProfileMenu';
+
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 h-16 w-full bg-white shadow-md"></header>
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center bg-white shadow-md">
+      <div className="flex w-full items-center justify-between px-8">
+        <div className="flex items-center gap-8">
+          <div>
+            <Link href="/">Logo</Link>
+          </div>
+          <div>
+            <Link href="/about">How it works</Link>
+          </div>
+        </div>
+        <div className="flex items-center gap-8">
+          <div>
+            <Link href="/basket">
+              <LuShoppingBasket size={20} />
+            </Link>
+          </div>
+
+          <HeaderProfileMenu />
+        </div>
+      </div>
+    </header>
   );
 }
