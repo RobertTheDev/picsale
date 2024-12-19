3f07fecc1d40d662a5677e80258ef10226fb1311
Robert Hawker <robertthedev@gmail.com>
2024-12-19 07:32:57 +0000

    feat: added new background color

--- src/app/(photos)/page.tsx
+++ src/app/(photos)/page.tsx
@@ -3,7 +3,7 @@ import PhotoProductCard from './components/PhotoProductCard';
 
 export default function PhotosPage() {
   return (
-    <main className="w-full">
+    <main className="w-full bg-gray-50">
       <ul className="grid w-full grid-cols-4 gap-8 p-8">
         {photos.map((photo) => {
           return <PhotoProductCard key={photo.id} photo={photo} />;
