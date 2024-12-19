df981def4f99335066165c8f8cc043954c4bf4d8
Robert Hawker <robertthedev@gmail.com>
2024-12-19 07:21:51 +0000

    feat: mapped photo product card component

--- src/app/(photos)/page.tsx
+++ src/app/(photos)/page.tsx
@@ -1,7 +1,14 @@
+import { photos } from '../data/photos';
+import PhotoProductCard from './components/PhotoProductCard';
+
 export default function PhotosPage() {
   return (
-    <div>
-      <h1>Photos</h1>
-    </div>
+    <main className="w-full">
+      <ul className="grid w-full grid-cols-4 gap-8 p-8">
+        {photos.map((photo) => {
+          return <PhotoProductCard key={photo.id} photo={photo} />;
+        })}
+      </ul>
+    </main>
   );
 }
