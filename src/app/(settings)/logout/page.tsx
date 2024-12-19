21ab5bb505d35b4b954d6cbe536d59a297ba6509
Robert Hawker <robertthedev@gmail.com>
2024-12-19 10:43:44 +0000

    feat: created logout page

---
+++ src/app/(settings)/logout/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Logout',
+};
+
+export default function LogoutPage() {
+  return (
+    <main>
+      <h1>Logout</h1>
+    </main>
+  );
+}
