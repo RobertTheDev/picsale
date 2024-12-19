12c42b5de8f42862871ae8d233bfe1a4782884b7
Robert Hawker <robertthedev@gmail.com>
2024-12-19 07:40:44 +0000

    feat: added filter and photo data to photo product page

--- src/app/(photos)/photos/[slug]/page.tsx
+++ src/app/(photos)/photos/[slug]/page.tsx
@@ -1,7 +1,14 @@
+import { photos } from '@/app/data/photos';
+import Image from 'next/image';
+
 export default function PhotoPage({ params }: { params: { slug: string } }) {
+  const photo = photos.find((photo) => photo.id === params.slug);
+
   return (
     <main>
-      <h1>Photo {params.slug}</h1>
+      <div className="relative h-96 w-96">
+        <Image fill objectFit="cover" alt="" src={`/photos/${photo?.url}`} />
+      </div>
     </main>
   );
 }
