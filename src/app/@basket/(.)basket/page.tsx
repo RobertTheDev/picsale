594c1b46f474fa67626cfd7700795d89b654ddcd
Robert Hawker <robertthedev@gmail.com>
2024-12-19 15:28:41 +0000

    chore: added document overflow hidden effect

--- src/app/@basket/(.)basket/page.tsx
+++ src/app/@basket/(.)basket/page.tsx
@@ -1,19 +1,34 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Page() {
   const router = useRouter();
 
+  const pathName = usePathname();
+
+  useEffect(() => {
+    if (pathName === '/basket') {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
+
   return (
     <div className="fixed left-0 top-0 z-[66] flex min-h-screen min-w-full items-center justify-center bg-[rgba(0,0,0,0.5)]">
-      <div className="bg-white">
+      <div className="fixed right-0 min-h-screen w-96 bg-white">
         <button
           onClick={() => {
             router.back();
           }}
         >
-          Close
+          Close basket
         </button>
       </div>
     </div>
