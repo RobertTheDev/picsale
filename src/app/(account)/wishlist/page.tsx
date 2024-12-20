e33aa6ce5d0838c61d7f7982c92c036ef07b04c1
Robert Hawker <robertthedev@gmail.com>
2024-12-20 07:59:27 +0000

    feat: mapped data cards in wishlist page

--- src/app/(account)/wishlist/page.tsx
+++ src/app/(account)/wishlist/page.tsx
@@ -1,4 +1,7 @@
+import { photos } from '@/app/data/photos';
 import { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Wishlist',
@@ -6,8 +9,28 @@ export const metadata: Metadata = {
 
 export default function WishlistPage() {
   return (
-    <main>
-      <h1>Wishlist</h1>
+    <main className="px-8">
+      <h1 className="mt-8 text-3xl font-bold">Wishlist</h1>
+      <div className="mt-8 flex w-full flex-col">
+        <ul className="grid w-full grid-cols-4 gap-8">
+          {photos.map((photo) => {
+            return (
+              <li className="w-full" key={photo.id}>
+                <Link href={`/photos/${photo.id}`}>
+                  <div className="relative aspect-square w-full">
+                    <Image
+                      fill
+                      objectFit="cover"
+                      alt=""
+                      src={`/photos/${photo.url}`}
+                    />
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </main>
   );
 }
