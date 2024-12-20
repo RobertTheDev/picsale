3dfc191fe5a7351eb0dc27ec103e075a4b7243dd
Robert Hawker <robertthedev@gmail.com>
2024-12-20 12:09:50 +0000

    feat: added content to photo intercepting route

--- src/app/@photos/(.)photos/[slug]/page.tsx
+++ src/app/@photos/(.)photos/[slug]/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import { photos } from '@/app/data/photos';
+import Image from 'next/image';
+import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { LuHeart, LuShoppingBasket } from 'react-icons/lu';
 
 export default function Page({ params }: { params: { slug: string } }) {
   const router = useRouter();
@@ -20,18 +24,54 @@ export default function Page({ params }: { params: { slug: string } }) {
     };
   }, [pathName]);
 
+  const photo = photos.find((p) => p.id === params.slug);
+
   return (
     <div className="fixed left-0 top-0 z-[66] flex min-h-screen min-w-full items-center justify-center bg-[rgba(0,0,0,0.5)]">
-      <div className="bg-white">
-        <h1>Photos {params.slug}</h1>
-        <button
-          onClick={() => {
-            router.back();
-          }}
-        >
-          Close
-        </button>
-      </div>
+      <main className="flex gap-8 bg-white p-8">
+        <div className="relative aspect-square w-1/2">
+          <Image fill objectFit="cover" alt="" src={`/photos/${photo?.url}`} />
+        </div>
+        <div className="flex flex-col">
+          <div className="bg-white">
+            <button
+              onClick={() => {
+                router.back();
+              }}
+            >
+              Close
+            </button>
+          </div>
+          <h2 className="text-4xl font-bold">{photo?.title}</h2>
+          <p>{photo?.description}</p>
+          <p>£{photo?.price.amount}</p>
+          <Link href={`/sellers/${photo?.seller.id}`}>
+            <div className="flex items-center gap-4">
+              <div className="relative size-16 overflow-hidden rounded-full">
+                <Image
+                  objectFit="cover"
+                  fill
+                  src={photo!.seller.imageUrl}
+                  alt=""
+                />
+              </div>
+              <div>
+                <p>
+                  {photo?.seller.firstName} {photo?.seller.lastName}
+                </p>
+              </div>
+            </div>
+          </Link>
+          <div className="flex items-center gap-4">
+            <button className="flex items-center justify-center gap-4 rounded-lg bg-black px-4 py-4 text-white">
+              <LuHeart size={20} /> Save to wishlist
+            </button>
+            <button className="flex items-center justify-center gap-4 rounded-lg bg-black px-4 py-4 text-white">
+              <LuShoppingBasket size={20} /> Add to basket
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
