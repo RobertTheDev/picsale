2dccc88ada8eb52dcdb6053688ed11fab7c92405
Robert Hawker <robertthedev@gmail.com>
2024-12-21 13:58:14 +0000

    refactor: updated icon sizes

--- src/app/(home)/components/PhotoFilterMenu/index.tsx
+++ src/app/(home)/components/PhotoFilterMenu/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useCallback } from 'react';
+import Link from 'next/link';
 import {
   LuGift,
   LuGraduationCap,
@@ -22,7 +21,7 @@ const categories = [
     description:
       'Explore stunning visuals of animals, landscapes, and the beauty of nature.',
     slug: 'nature-wildlife',
-    icon: <LuSquirrel size={20} />,
+    icon: <LuSquirrel size={24} />,
   },
   {
     id: 2,
@@ -31,7 +30,7 @@ const categories = [
       'Capture moments of human connection, fashion, and daily life.',
     slug: 'lifestyle-people',
     image: 'lifestyle-people.jpg',
-    icon: <LuUsers size={20} />,
+    icon: <LuUsers size={24} />,
   },
   {
     id: 3,
@@ -39,14 +38,14 @@ const categories = [
     description:
       'Innovative images showcasing corporate culture and the latest in tech.',
     slug: 'business-technology',
-    icon: <LuLaptop size={20} />,
+    icon: <LuLaptop size={24} />,
   },
   {
     id: 4,
     title: 'Food & Drink',
     description: 'Delicious images of food, beverages, and culinary delights.',
     slug: 'food-drink',
-    icon: <LuPizza size={20} />,
+    icon: <LuPizza size={24} />,
   },
   {
     id: 5,
@@ -54,7 +53,7 @@ const categories = [
     description:
       'Wanderlust-inducing scenes of famous landmarks and outdoor adventures.',
     slug: 'travel-landmarks',
-    icon: <LuLuggage size={20} />,
+    icon: <LuLuggage size={24} />,
   },
   {
     id: 6,
@@ -62,14 +61,14 @@ const categories = [
     description:
       'Creative artworks, patterns, and cultural designs for inspiration.',
     slug: 'art-design',
-    icon: <LuPalette size={20} />,
+    icon: <LuPalette size={24} />,
   },
   {
     id: 7,
     title: 'Health & Wellness',
     description: 'Images promoting health, fitness, and medical care.',
     slug: 'health-wellness',
-    icon: <LuHeartPulse size={20} />,
+    icon: <LuHeartPulse size={24} />,
   },
   {
     id: 8,
@@ -77,7 +76,7 @@ const categories = [
     description:
       'Action-packed visuals of athletes, sports, and recreational activities.',
     slug: 'sports-recreation',
-    icon: <LuVolleyball size={20} />,
+    icon: <LuVolleyball size={24} />,
   },
   {
     id: 9,
@@ -85,54 +84,31 @@ const categories = [
     description:
       'Resources and imagery focused on education, learning, and growth.',
     slug: 'education-learning',
-    icon: <LuGraduationCap size={20} />,
+    icon: <LuGraduationCap size={24} />,
   },
   {
     id: 10,
     title: 'Holidays & Celebrations',
     description: 'Festive images celebrating holidays and special occasions.',
     slug: 'holidays-celebrations',
-    icon: <LuGift size={20} />,
+    icon: <LuGift size={24} />,
   },
 ];
 
 export default function PhotoFilterMenu() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams.toString());
-      params.set(name, value);
-
-      return params.toString();
-    },
-    [searchParams],
-  );
-
   return (
-    <div className="flex max-h-[80vh] flex-col overflow-hidden overflow-y-scroll">
-      <div>
-        <h2 className="text-lg font-bold">Categories</h2>
-      </div>
-      <div className="mt-8 flex flex-col gap-8">
+    <div className="flex flex-col overflow-hidden overflow-y-scroll px-4">
+      <div className="mt-6 flex flex-col gap-3">
         {categories.map((category) => {
           return (
-            <button
+            <Link
+              href={`/categories/${category.slug}`}
               key={category.slug}
-              className="flex items-center gap-4 hover:bg-gray-300"
-              onClick={() => {
-                router.push(
-                  pathname + '?' + createQueryString('category', category.slug),
-                );
-              }}
+              className="flex h-12 w-full items-center gap-4 overflow-hidden rounded-lg px-4 py-4 text-base hover:bg-gray-300"
             >
-              <div className="relative flex size-10 items-center justify-center rounded-full bg-gray-300">
-                {category.icon}
-              </div>
-              {category.title}
-            </button>
+              <div className="relative">{category.icon}</div>
+              <p className="text-base font-medium">{category.title}</p>
+            </Link>
           );
         })}
       </div>
