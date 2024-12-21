219d6e3b75455970d76c72593d42513887d159eb
Robert Hawker <robertthedev@gmail.com>
2024-12-21 13:54:36 +0000

    feat: created and mapped basket item card component

--- src/app/(basket)/basket/page.tsx
+++ src/app/(basket)/basket/page.tsx
@@ -1,7 +1,6 @@
 import { photos } from '@/app/data/photos';
 import { Metadata } from 'next';
-import Image from 'next/image';
-import { LuTrash2 } from 'react-icons/lu';
+import BasketItemCard from './components/BasketItemCard';
 
 export const metadata: Metadata = {
   title: 'Basket',
@@ -16,27 +15,7 @@ export default function BasketPage() {
         </div>
         <div className="mt-8 flex w-full flex-col items-center gap-8 pb-20">
           {photos.map((photo) => {
-            return (
-              <div key={photo.id} className="flex w-full items-start gap-4">
-                <div className="relative aspect-square size-16 overflow-hidden rounded-lg">
-                  <Image
-                    objectFit="cover"
-                    src={`/photos/${photo.url}`}
-                    fill
-                    alt=""
-                  />
-                </div>
-                <div className="flex flex-col items-start">
-                  <p className="text-lg font-bold">Mountain Landscape</p>
-                  <p className="text-base font-medium">£215.00</p>
-                </div>
-                <div>
-                  <button>
-                    <LuTrash2 />
-                  </button>
-                </div>
-              </div>
-            );
+            return <BasketItemCard key={photo.id} basketItem={photo} />;
           })}
         </div>
         <div className="fixed bottom-0 bg-white py-4">
