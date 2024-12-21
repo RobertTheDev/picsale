0ea34b2437a859d519ce747098aefaeaf2858826
Robert Hawker <robertthedev@gmail.com>
2024-12-21 14:51:12 +0000

    feat: added photos field

--- src/app/interfaces/Seller.ts
+++ src/app/interfaces/Seller.ts
@@ -1,6 +1,9 @@
+import type IPhoto from './Photo';
+
 export default interface ISeller {
   id: string;
   firstName: string;
-  imageUrl: string;
+  imageUrl: string | null;
   lastName: string;
+  photos: IPhoto[];
 }
