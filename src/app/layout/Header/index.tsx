9bbb5066593b00f72c72637bf5114915aa0a6a3f
Robert Hawker <robertthedev@gmail.com>
2024-12-21 13:55:37 +0000

    feat: added responsive styles

--- src/app/layout/Header/index.tsx
+++ src/app/layout/Header/index.tsx
@@ -6,47 +6,47 @@ import HeaderProfileMenu from './components/HeaderProfileMenu';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full items-center bg-white">
-      <div className="flex w-full items-center justify-between px-8">
+    <header className="sticky top-0 z-50 flex h-16 w-full items-center bg-white shadow-md">
+      <div className="flex w-full items-center justify-between px-4 md:px-8">
         <div className="flex items-center gap-8">
-          <div>
-            <Link href="/">
-              <div className="flex items-center gap-2">
-                <div className="relative size-8">
-                  <Image fill src="/logo.png" alt="" />
-                </div>
-                <div>
-                  <p className="text-xl font-bold">
-                    {company.name.toLowerCase()}
-                  </p>
-                </div>
+          <Link href="/">
+            <div className="flex items-center gap-2">
+              <div className="relative size-10">
+                <Image fill src="/logo.png" alt="" />
               </div>
-            </Link>
-          </div>
-          <div>
+              <div>
+                <p className="text-xl font-bold">
+                  {company.name.toLowerCase()}
+                </p>
+              </div>
+            </div>
+          </Link>
+          <div className="hidden md:flex">
             <Link href="/about">How it works</Link>
           </div>
         </div>
-        <div className="flex items-center gap-8">
+        <div className="flex items-center gap-5">
           <div>
             <Link href="/basket">
               <div className="relative">
-                <div className="absolute right-[-6px] top-[-6px] flex size-4 items-center justify-center rounded-full bg-blue-600">
-                  <p className="text-sm text-white">4</p>
+                <div className="absolute right-[-12px] top-[-8px] flex size-6 items-center justify-center rounded-full bg-[#0d95e1]">
+                  <p className="text-sm font-medium text-white">9</p>
                 </div>
                 <div>
-                  <LuShoppingBasket size={24} />
+                  <LuShoppingBasket size={28} />
                 </div>
               </div>
             </Link>
           </div>
           <HeaderProfileMenu />
-          <Link
-            href="/sign-in"
-            className="flex items-center justify-center rounded-lg bg-black px-4 py-2 text-sm font-medium text-white"
-          >
-            Sign In
-          </Link>
+          <div className="hidden">
+            <Link
+              href="/sign-in"
+              className="flex items-center justify-center rounded-lg bg-black px-4 py-2 text-sm font-medium text-white"
+            >
+              Sign In
+            </Link>
+          </div>
         </div>
       </div>
     </header>
