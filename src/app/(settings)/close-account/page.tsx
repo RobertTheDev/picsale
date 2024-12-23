ea1f019ea020294937bc3c2ada092c447204e69d
Robert Hawker <robertthedev@gmail.com>
2024-12-23 16:04:15 +0000

    feat: added form component to close account page

--- src/app/(settings)/close-account/page.tsx
+++ src/app/(settings)/close-account/page.tsx
@@ -1,29 +1,10 @@
 import { Metadata } from 'next';
+import CloseAccountForm from './components/CloseAccountForm';
 
 export const metadata: Metadata = {
   title: 'Close Account',
 };
 
 export default function CloseAccountPage() {
-  return (
-    <div>
-      <form className="mt-4 flex h-auto flex-col rounded-lg p-8">
-        <h3 className="text-base font-bold">Close Account</h3>
-        <p className="mt-4">
-          Your account will be permanently deleted along with its data. You will
-          lose all of your progresses, favorites, certificates, history and
-          profile information.
-        </p>
-
-        <div className="mt-4 flex flex-1 flex-col">
-          <button
-            type="submit"
-            className="w-full rounded-md bg-red-500 p-3 text-base font-bold text-white"
-          >
-            Close Account
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+  return <CloseAccountForm />;
 }
