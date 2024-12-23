e800bc66a21961ff8e10bd4d64bd93265f39d641
Robert Hawker <robertthedev@gmail.com>
2024-12-23 21:53:29 +0000

    chore: removed controls container in basket page

--- src/app/(basket)/basket/page.tsx
+++ src/app/(basket)/basket/page.tsx
@@ -9,40 +9,14 @@ export const metadata: Metadata = {
 export default function BasketPage() {
   return (
     <main className="flex w-full flex-col items-center">
-      <div className="w-full bg-neutral-900 px-8 py-8">
+      <div className="w-full bg-neutral-900 px-4 py-8">
         <div className="flex items-center justify-between">
-          <h1 className="text-xl font-bold">Your Basket</h1>
+          <h1 className="text-2xl font-bold">Your Basket</h1>
         </div>
-        <div className="mt-8 flex w-full flex-col items-center gap-8 pb-20">
+        <div className="mt-8 flex w-full flex-col items-center gap-8">
           {photos.map((photo) => {
             return <BasketItemCard key={photo.id} basketItem={photo} />;
           })}
-        </div>
-        <div className="fixed bottom-0 bg-white py-4">
-          <div className="flex flex-col">
-            <div className="flex h-12 items-center justify-between">
-              <div>
-                <p className="font-bold">Subtotal</p>
-              </div>
-              <div>
-                <p className="font-bold">£430.00</p>
-              </div>
-            </div>
-            <div className="mt-4 flex w-full items-center gap-4">
-              <button
-                type="button"
-                className="h-12 w-40 overflow-hidden rounded-md bg-black font-medium text-white"
-              >
-                View Basket
-              </button>
-              <button
-                type="button"
-                className="h-12 w-40 overflow-hidden rounded-md bg-green-500 font-medium text-white"
-              >
-                Checkout
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </main>
