54bab4d9dd636458f9fa46c4e1a5e18975cd6d00
Robert Hawker <robertthedev@gmail.com>
2024-12-23 16:01:47 +0000

    feat: added new styles to settings layout

--- src/app/(settings)/layout.tsx
+++ src/app/(settings)/layout.tsx
@@ -4,11 +4,13 @@ import SettingsMenu from './components/SettingsMenu';
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="flex w-full">
-      <aside className="fixed top-0 hidden h-screen pt-16 md:flex md:w-80 2xl:w-80">
+      <aside className="fixed top-0 hidden h-screen pt-16 md:flex md:w-80">
         <SettingsMenu />
       </aside>
-      <div className="flex w-full flex-1 flex-col md:ml-80 2xl:ml-96">
-        {children}
+      <div className="mt-8 flex w-full flex-1 flex-col items-center px-4 md:ml-80">
+        <div className="w-full overflow-hidden rounded-lg bg-neutral-900 md:w-3/5">
+          {children}
+        </div>
       </div>
     </main>
   );
