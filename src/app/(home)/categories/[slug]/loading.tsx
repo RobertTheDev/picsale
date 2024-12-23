00b475ff9e0059470b58d087193d8bb1f038063a
Robert Hawker <robertthedev@gmail.com>
2024-12-23 15:57:26 +0000

    feat: created category page loader

---
+++ src/app/(home)/categories/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from '@/app/layout/Spinner';
+
+export default function Loading() {
+  return (
+    <div className="flex w-full flex-col items-center pt-16">
+      <Spinner />
+    </div>
+  );
+}
