0741582994ff210cc9db43663ff9fab207e59b26
Robert Hawker <robertthedev@gmail.com>
2024-12-24 18:55:09 +0000

    refactor: added responsive styles to tab bar and mobile header

--- src/app/(home)/components/MobileSearchHeader/index.tsx
+++ src/app/(home)/components/MobileSearchHeader/index.tsx
@@ -4,7 +4,7 @@ import { LuSearch } from 'react-icons/lu';
 
 export default function MobileSearchHeader() {
   return (
-    <div className="sticky top-0 z-50 flex h-16 items-center justify-between bg-[rgba(0,0,0,0.8)] px-4">
+    <div className="sticky top-0 z-50 flex h-16 items-center justify-between bg-[rgba(0,0,0,0.8)] px-4 lg:hidden">
       <div className="flex items-center gap-2">
         <div className="relative size-9">
           <Image fill src="/logo.png" alt="" />
--- src/app/layout/TabBar/index.tsx
+++ src/app/layout/TabBar/index.tsx
@@ -3,7 +3,7 @@ import { tabBarLinks } from './lib/tabBarLinks';
 
 export default function TabBar() {
   return (
-    <div className="fixed bottom-0 flex h-16 w-full items-center bg-[rgba(0,0,0,1)]">
+    <div className="fixed bottom-0 flex h-16 w-full items-center bg-[rgba(0,0,0,1)] lg:hidden">
       {tabBarLinks.map((tabBarLink) => {
         return <TabBarLink key={tabBarLink.href} tabBarLink={tabBarLink} />;
       })}
