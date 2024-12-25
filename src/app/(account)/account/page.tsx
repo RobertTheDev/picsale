83f33c6127f2ead1354af3d5f5d1edf7f0580aff
Robert Hawker <robertthedev@gmail.com>
2024-12-25 00:10:47 +0000

    feat: added account menu links

--- src/app/(account)/account/page.tsx
+++ src/app/(account)/account/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { LuChevronRight } from 'react-icons/lu';
-import profileMenuLinks from '../../layout/Header/components/HeaderProfileMenu/profileMenuLinks';
 import TabBar from '../../layout/TabBar';
+import accountMenuLinks from './accountMenuLinks';
 
 export default function AccountPage() {
   return (
@@ -14,13 +14,16 @@ export default function AccountPage() {
           </div>
           <p className="mt-4 text-2xl font-bold text-white">Robert Hawker</p>
         </div>
-        <div className="mt-8 w-full">
-          <ul className="flex w-full flex-col items-center gap-4">
-            {profileMenuLinks.map((link) => {
+        <div className="mt-8 flex w-full flex-col items-center overflow-hidden rounded-lg">
+          <ul className="flex w-full flex-col items-center bg-neutral-800">
+            {accountMenuLinks.map((link) => {
               return (
-                <li key={link.href} className="w-full">
+                <li
+                  key={link.href}
+                  className="w-full border-b-[1px] border-b-neutral-600 last:border-b-0"
+                >
                   <Link
-                    className="flex h-12 w-full items-center justify-between rounded-lg bg-neutral-800 px-4 hover:bg-gray-800"
+                    className="flex h-12 w-full items-center justify-between px-4"
                     href={link.href}
                   >
                     <div className="flex items-center gap-5">
@@ -28,13 +31,18 @@ export default function AccountPage() {
                       <p>{link.name}</p>
                     </div>
                     <div>
-                      <LuChevronRight size={28} />
+                      <LuChevronRight size={24} />
                     </div>
                   </Link>
                 </li>
               );
             })}
           </ul>
+        </div>
+        <div>
+          <button type="button" className="mt-8 text-red-500">
+            Log out
+          </button>
         </div>
       </div>
       <TabBar />
