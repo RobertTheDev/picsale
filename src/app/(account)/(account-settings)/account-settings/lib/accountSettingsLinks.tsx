bbc6aed8789b02a15e97303a8a679425c396ea93
Robert Hawker <robertthedev@gmail.com>
2024-12-25 00:10:20 +0000

    refactor: updated ion sizes

--- src/app/(account)/(account-settings)/account-settings/lib/accountSettingsLinks.tsx
+++ src/app/(account)/(account-settings)/account-settings/lib/accountSettingsLinks.tsx
@@ -4,32 +4,32 @@ const accountSettingsLinks = [
   {
     id: 1,
     name: 'Edit Profile',
-    slug: 'edit-profile',
-    icon: <LuPencil size={20} />,
+    slug: '/edit-profile',
+    icon: <LuPencil size={24} />,
   },
   {
     id: 2,
     name: 'Change Email',
-    slug: 'change-email',
-    icon: <LuMail size={20} />,
+    slug: '/change-email',
+    icon: <LuMail size={24} />,
   },
   {
     id: 3,
     name: 'Change Password',
-    slug: 'change-password',
-    icon: <LuLock size={20} />,
+    slug: '/change-password',
+    icon: <LuLock size={24} />,
   },
   {
     id: 4,
     name: 'Verify Email',
-    slug: 'verify-email',
-    icon: <LuMailCheck size={20} />,
+    slug: '/verify-email',
+    icon: <LuMailCheck size={24} />,
   },
   {
     id: 5,
     name: 'Close Account',
-    slug: 'close-account',
-    icon: <LuTrash size={20} />,
+    slug: '/close-account',
+    icon: <LuTrash size={24} />,
   },
 ];
 
