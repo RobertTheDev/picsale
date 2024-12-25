ba7ea21e86857585ccb861d69ac8ddc2fe8c1c3c
Robert Hawker <robertthedev@gmail.com>
2024-12-25 00:09:33 +0000

    feat: added add to basket button

--- src/app/(home)/(search)/components/PhotoProductCard/index.tsx
+++ src/app/(home)/(search)/components/PhotoProductCard/index.tsx
@@ -1,22 +1,33 @@
 import IPhoto from '@/app/interfaces/Photo';
 import Image from 'next/image';
 import Link from 'next/link';
+import { LuShoppingBasket } from 'react-icons/lu';
 
-export default async function PhotoProductCard({ photo }: { photo: IPhoto }) {
+export default function PhotoProductCard({ photo }: { photo: IPhoto }) {
   return (
-    <Link href={`/photos/${photo.id}`}>
-      <li className="relative w-full">
-        <div className="relative aspect-square w-full">
+    <Link href={`/purchases/${photo.id}`}>
+      <div className="flex w-full gap-6">
+        <div className="relative aspect-square h-28 overflow-hidden rounded-xl">
           <Image
-            loading="eager"
-            className="object-cover"
             fill
-            alt=""
             src={`/photos/${photo.url}`}
-            priority
+            alt="Photo preview"
+            className="object-cover"
           />
         </div>
-      </li>
+        <div className="flex flex-1 flex-col">
+          <p className="text-base font-bold text-white">
+            Image of a gorilla lying down
+          </p>
+          <button
+            type="button"
+            className="mt-2 flex h-12 w-full items-center justify-center gap-2 rounded-full bg-neutral-800 text-sm font-medium text-white"
+          >
+            <LuShoppingBasket size={20} />
+            Add To Basket
+          </button>
+        </div>
+      </div>
     </Link>
   );
 }
