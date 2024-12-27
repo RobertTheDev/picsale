88ca09d9c989052aae7496b166479510cce38f7a
Robert Hawker <robertthedev@gmail.com>
2024-12-27 22:20:48 +0000

    feat: defined baskets data

---
+++ src/app/data/baskets.ts
@@ -0,0 +1,49 @@
+export const baskets = [
+  {
+    id: '1',
+    currency: 'GBP',
+    amount: 23000,
+    items: [
+      {
+        id: '1',
+        url: '1.jpg',
+        category: 'nature-wildlife',
+        title: 'Golf Course in Portugal',
+        description: 'This is the description',
+        price: { currency: 'GBP', amount: 1099 },
+      },
+      {
+        id: '2',
+        url: '2.jpg',
+        category: 'nature-wildlife',
+        title: 'Golf Course in Portugal',
+        description: 'This is the description',
+        price: { currency: 'GBP', amount: 1099 },
+      },
+      {
+        id: '3',
+        url: '3.jpg',
+        category: 'nature-wildlife',
+        title: 'Golf Course in Portugal',
+        description: 'This is the description',
+        price: { currency: 'GBP', amount: 1099 },
+      },
+      {
+        id: '4',
+        url: '4.jpg',
+        category: 'nature-wildlife',
+        title: 'Golf Course in Portugal',
+        description: 'This is the description',
+        price: { currency: 'GBP', amount: 1099 },
+      },
+      {
+        id: '5',
+        url: '5.jpg',
+        category: 'travel-landmarks',
+        title: 'Golf Course in Portugal',
+        description: 'This is the description',
+        price: { currency: 'GBP', amount: 1099 },
+      },
+    ],
+  },
+];
