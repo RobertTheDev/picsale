e401eb7d4b4b5e1b4a22707029d9a42da759dcd0
Robert Hawker <robertthedev@gmail.com>
2024-12-27 22:19:46 +0000

    fix: fixed reset password form import path

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Suspense } from 'react';
-import ResetPasswordForm from './components/ResetPasswordForm';
+import ResetPasswordForm from '../components/ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
