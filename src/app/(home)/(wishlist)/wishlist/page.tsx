2ddd92afbb5c30971eac56ca77c524e734c79404
Robert Hawker <robertthedev@gmail.com>
2024-12-27 22:20:17 +0000

    feat: created wishlist page

---
+++ src/app/(home)/(wishlist)/wishlist/page.tsx
@@ -0,0 +1,24 @@
+import PhotoProductCard from '@/app/(home)/(search)/components/PhotoProductCard';
+import { photos } from '@/app/data/photos';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Wishlist',
+};
+
+export default function WishlistPage() {
+  return (
+    <main className="flex flex-col px-4 pb-8">
+      <div className="mt-8">
+        <h1 className="text-3xl font-bold">Wishlist</h1>
+      </div>
+      <div className="mt-8 flex w-full flex-col">
+        <ul className="grid w-full grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {photos.map((photo) => {
+            return <PhotoProductCard key={photo.id} photo={photo} />;
+          })}
+        </ul>
+      </div>
+    </main>
+  );
+}
